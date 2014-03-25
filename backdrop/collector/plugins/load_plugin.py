1bff094e8c3f6be69468fdf301db9f1d9effd4b6
Peter Waller <peter@scraperwiki.com>
2014-03-25 14:28:30 +0000

    Implement better tests for load_plugin

--- backdrop/collector/plugins/load_plugin.py
+++ backdrop/collector/plugins/load_plugin.py
@@ -6,14 +6,38 @@ Responsible for taking plugin strings and returning plugin callables.
 
 """
 
+# For the linter
+import __builtin__
+
+import backdrop.collector.plugins
+
+from backdrop.collector.plugins import AggregateKey, ComputeDepartmentKey
+
 
 def load_plugins(plugin_names):
     return [load_plugin(plugin_name) for plugin_name in plugin_names]
 
 
 def load_plugin(plugin_name):
-    pass
+    return NotImplemented
 
 
-def test_load_plugin():
-    pass
+def test_load_plugin_trivial():
+    from nose.tools import assert_equal
+
+    assert_equal(1, load_plugin("1"))
+
+
+def test_load_plugin_compute_department_key():
+    from nose.tools import assert_is_instance
+
+    plugin = load_plugin('ComputeDepartmentKey("customVarValue9")')
+    assert_is_instance(plugin, ComputeDepartmentKey)
+
+
+def test_load_plugin_compute_aggregate_key():
+    from nose.tools import assert_is_instance
+
+    plugin = load_plugin('AggregateKey(aggregate_count("visits"),'
+                         '             aggregate_rate("rate", "visits"))')
+    assert_is_instance(plugin, AggregateKey)
