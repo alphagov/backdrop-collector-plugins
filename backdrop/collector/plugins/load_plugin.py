086ab0f09029a1f309230d105f2eb81c553d427a
Peter Waller <peter@scraperwiki.com>
2014-03-25 14:07:07 +0000

    Forget about plugin string parsing for now.

--- backdrop/collector/plugins/load_plugin.py
+++ backdrop/collector/plugins/load_plugin.py
@@ -4,23 +4,6 @@ load_plugin.py
 
 Responsible for taking plugin strings and returning plugin callables.
 
-Examples of plugins:
-
-    # Identity, does nothing
-    `__builtins__:(lambda docs: docs)`
-
-    # If `docs` were integers, this would sum them.
-    `__builtins__:sum`
-
-    `backdrop.collector.plugins:ComputeDepartment()`
-    `backdrop.collector.plugins:AggregateKey()`
-
-Strings consist of an (optional) module-part, a colon, and an expression-part.
-
-If the string does not begin with something module-like ending in a `:`, then
-it is assumed that the plugin resides in `backdrop.collector.plugins`.
-
-The expression-part is evaluated with a local namespace of the module.
 """
 
 
@@ -34,31 +17,3 @@ def load_plugin(plugin_name):
 
 def test_load_plugin():
     pass
-
-
-def parse_plugin_string(plugin_string):
-    """
-    Determine module-part and expression-part of `plugin_string`
-    """
-
-PLUGIN_PARSE_TESTS = [
-    ["a.b.c:expression.foo", ("a.b.c", "expression.foo")],
-    ["expression.foo", ("backdrop.collector.plugins", "expression.foo")],
-
-    ['expression.foo("bar: baz")',
-     ("backdrop.collector.plugins", 'expression.foo("bar: baz")')],
-
-    ['module.foo:expression.foo("bar: baz")',
-     ("module.foo", 'expression.foo("bar: baz")')],
-]
-
-
-def test_parse_plugin_string():
-    from nose.tools import assert_equal
-
-    def check(input_, expected):
-        output = parse_plugin_string(input_)
-        assert_equal(output, expected)
-
-    for input_, expected in PLUGIN_PARSE_TESTS:
-        yield check, input_, expected
