5672529898a3507c7e188c2517f691d8fe1b1d9c
Peter Waller <p@pwaller.net>
2014-04-01 11:50:56 +0100

    Switch to using pkgutil.extend_path
    This is for consistency with alphagov/backdrop-collector#15,
    and fixes an issue I was having where I couldn't find
    backdrop.collector.plugins because my environment had changed
    very slightly.

--- backdrop/collector/plugins/__init__.py
+++ backdrop/collector/plugins/__init__.py
@@ -1,3 +1,7 @@
+# Namespace package: https://docs.python.org/2/library/pkgutil.html
+from pkgutil import extend_path
+__path__ = extend_path(__path__, __name__)
+
 from .aggregate import AggregateKey, aggregate_count, aggregate_rate
 from .department import ComputeDepartmentKey
 from .remove_key import RemoveKey
