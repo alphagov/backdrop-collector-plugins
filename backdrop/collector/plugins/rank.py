fcb2e1f705bae02ad01dfafc646357d07ed2961b
Peter Waller <peter@scraperwiki.com>
2014-04-14 14:19:20 +0100

    Implement ComputeRank

--- backdrop/collector/plugins/rank.py
+++ backdrop/collector/plugins/rank.py
@@ -5,6 +5,8 @@ class ComputeRank(object):
         self.var_name = var_name
 
     def __call__(self, documents):
+        for i, document in enumerate(documents, 1):
+            document[self.var_name] = i
         return documents
 
 def test_rank():
