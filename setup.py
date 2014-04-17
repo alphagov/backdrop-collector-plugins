0427a799086d25f25ec8440ca8aa0ac25f2bcc41
Rob Young <rob.young@digital.cabinet-office.gov.uk>
2014-04-17 10:31:47 +0100

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='backdrop-collector-plugins',
-    version="1.0.0",
+    version="1.1.0",
     packages=["backdrop.collector.plugins"],
     namespace_packages=['backdrop', 'backdrop.collector'],
 
